d825b507df32e02a3e731a9cbd97eff82363f7aa
Omkar Reddy <omkarreddy@Omkars-MacBook-Pro-3.local>
2020-08-16 13:02:28 -0400

    Changing format of tree data json storage

--- gym-percolation/generate_trees.py
+++ gym-percolation/generate_trees.py
@@ -34,6 +34,35 @@ def whole_tree_training_data(root):
         output+=whole_tree_training_data(c)
     return output
 
+def format_json_data(json_data):
+
+    result_list = []
+    
+    for index in range(len(json_data['X'])):
+        temp_dict = {}
+        if len(json_data['X'][index]) != 0:
+            for i in range(len(json_data['Y_good'][index])):
+                 temp_dict = {}
+                 temp_dict['board_state'] = json_data['X'][index]
+                 temp_dict['action'] = json_data['Y_good'][index][i]
+                 temp_dict['move_quality'] = 1
+                 result_list.append(temp_dict)
+
+            for i in range(len(json_data['Y_bad'][index])):
+                 temp_dict = {}
+                 temp_dict['board_state'] = json_data['X'][index]
+                 temp_dict['action'] = json_data['Y_bad'][index][i]
+                 temp_dict['move_quality'] = -1
+                 result_list.append(temp_dict)
+
+    json_data = {'1': result_list}
+             
+    #file_path = './omkar.json'
+    #json.dump(json_data, codecs.open(file_path, 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+
+    print("Done")
+    return json_data
+
 def main():
 
     parser = argparse.ArgumentParser()
@@ -59,8 +88,12 @@ def main():
         print(sorted([i.moves_to_end for i in tree.children]))
         trees.append(tree)
         training_data += whole_tree_training_data(tree)
+
+    json_dict = dict(X=training_data[::3],Y_good=training_data[1::3],Y_bad=training_data[2::3])
+
+    json_data = format_json_data(json_dict)
     
-    json.dump(dict(X=training_data[::3],Y_good=training_data[1::3],Y_bad=training_data[2::3]), open(args.file_out,"w"))
+    json.dump(json_data, open(args.file_out,"w"),  separators=(',', ':'), sort_keys=True, indent=4)
     if args.make_plots:
         plot_data(trees[0],recurse=True)
     return trees
@@ -69,5 +102,3 @@ def main():
 
 if __name__ == "__main__":
     trees = main()
-    
-    
